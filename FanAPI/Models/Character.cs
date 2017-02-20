24d45f064233196969113746caa2b2aa8d4917b3
heymichaelw <heymichaelw@gmail.com>
2017-02-20 14:12:33 -0500

    set up initial models and connected API

---
+++ FanAPI/Models/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanAPI.Models
+{
+    class Character
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public Uri Allegiances { get; set; }
+        public Uri Books { get; set; }
+
+        public House AllegianceDetail(HttpClient client)
+        {
+            var response = client.GetAsync(Allegiances).Result;
+            House allegiance = response.Content.ReadAsAsync<House>().Result;
+            return allegiance;
+        }
+    }
+}
--- FanAPI/Program.cs
+++ FanAPI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,18 @@ namespace FanAPI
 {
     class Program
     {
+        private static HttpClient client = new HttpClient();
+
+        private static void SetUpClient()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("http://www.anapioficeandfire.com/api/");
+        }
+
         static void Main(string[] args)
         {
+
         }
     }
 }
